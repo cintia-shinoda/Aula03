e207245058c8b48c34e79b4c02a7704f7f60c6db
Cintia Shinoda <shinoda.c.i@gmail.com>
2021-03-01 16:10:15 -0300

    Implementação do operador E

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -3,20 +3,50 @@ import java.util.Scanner;
 public class OperadorE {
 
 	public static void main(String[] args) {
-		/*Fa�a um programa que verifique se o usu�rio digitado � igual a 'administrador' e se a senha digitada � igual a '123'*/
+		/* Faça um programa que verifique se o usuário digitado é igual a 'administrador' e se a senha digitada é igual a '123' */
 		Scanner leitor = new Scanner(System.in);
 		String usuario, senha;
-		System.out.println("SISTEMA DE LOGIN");
+		System.out.println("--- SISTEMA DE LOGIN ---");
 		System.out.println("Por favor, digite seu username:");
 		usuario = leitor.next();
 		System.out.println("Por favor, digite sua senha:");
 		senha = leitor.next();	
-		//Aqui � preciso criar um if que verifique as duas condi��es para o login ser bem sucedido
+		
+	
+		if (usuario.equals("administrador") && senha.equals("123")) {
+			System.out.println("Usuário logado");
+		} else {
+			System.out.println("Usuário ou senha incorreto/a");
+		}		
 		
 		
+
+/*
+ * Segurança problemática:
+ * 
+		 if (usuario.equals("administrador")) {
+		 	if (senha.equals("123")){
+		 		System.out.println("Usuário logado");
+		 	} else {
+		 		System.out.println("Senha incorreta");
+		 	}
+		 } else {
+			 System.out.println("Usuário incorreto");
+*/
+		
+				
+/*
+// Ignorar maiúsculas/minúsculas no usuário:
+		
+		if (usuario.equalsIgnoreCase("administrador") && senha.equals("123")) {
+			System.out.println("Usuário logado");
+		} else {
+			System.out.println("Usuário ou senha incorreto/a");
+		}
+*/		
 		
 		leitor.close();
-
+	
 	}
 
 }
